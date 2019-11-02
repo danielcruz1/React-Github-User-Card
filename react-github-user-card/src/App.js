698048516a8a6c14aeb3e70f6e555841e758ac34
Daniel Cruz <d.cruz.489@hotmail.com>
2019-11-01 21:22:08 -0600

    mvp complete

--- react-github-user-card/src/App.js
+++ react-github-user-card/src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
-
-
-
+import Profile from './Components/UserData';
+import Followers from './Components/Followers';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World!</h1>
+      <div className='app-app'>
+        <h1>Git Hub User Card</h1>
+        <Profile />
+        <h1>Followers</h1>
+        <Followers />
+      </div>
     </div>
   );
 }
---
+++ react-github-user-card/src/Components/Followers.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import axios from 'axios';
+
+class Followers extends React.Component {
+    constructor() {
+        super();
+        this.state={
+            followerdata: [],
+            username: 'danielcruz1'
+        }
+    }
+
+    componentDidMount() {
+        axios
+            .get(`https://api.github.com/users/${this.state.username}/followers`)
+            .then(res => {
+                this.setState({
+                    followerdata: res.data  
+                });
+                console.log(res.data);
+            })
+            .catch(err => {
+                console.log('NANI?!',err)
+            });
+    }
+
+    render() {
+        return (
+            <div className='follower-container'>
+               {this.state.followerdata.map (follower => (
+                    <div className='cards' key={follower.id}>
+                        <img src={follower.avatar_url} />
+                        <h2>{follower.login}</h2>
+                        <p>Github: <a href={follower.url}>{follower.html_url}</a></p>
+                    </div>
+               ))}
+            </div>
+        );
+    }
+
+
+}
+
+
+export default Followers; 
